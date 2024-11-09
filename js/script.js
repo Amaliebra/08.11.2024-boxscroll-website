6d9b9a9bbbfc1449948ebee0fa1bd4e4dfe18b81
Amaliebra <amaliebratland@gmail.com>
2024-11-09 16:55:11 +0100

    added more text to "useless" box

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('scroll');
-  
-      }
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll');
+        } else {
+            entry.target.classList.remove('scroll'); 
+        }
     });
-  });
+}, { threshold: 0.5 });
 
-observer.observe(document.querySelector('.shine, .shape, .last'));
+document.querySelectorAll('.shape, .shape2, .shape3').forEach(element => {
+    observer.observe(element);
+});
